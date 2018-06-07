84ff58aea25dd77f8d684727a6dc2b737ac84dda
Richard Möhn <richard.moehn@posteo.de>
2018-06-07 09:00:58 +0900

    Add test for the upcoming laziness feature

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+"""Tests for patchwork.
+
+So far there is only one test. (Unless someone has added tests without updating
+this docstring.) Execute it like this::
+
+$ cd <patchwork repo root>
+$ python3.6 tests/test_laziness.py
+
+We might add a better test runner later.
+"""
