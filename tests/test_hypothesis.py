8623358e6c67edc37dd8b5211afe09b8ca13c619
Richard Möhn <richard.moehn@posteo.de>
2018-06-27 10:12:33 +0900

    WIP Untidily implement a strategy for generating hypertext

--- tests/test_hypothesis.py
+++ tests/test_hypothesis.py
@@ -6,7 +6,48 @@ from patchwork.datastore import Datastore
 from patchwork.scheduling import RootQuestionSession, Scheduler
 
 
-hypertext = st.text().filter(lambda s: '[' not in s and '$' not in s)
+# [ starts an expanded pointer and $ starts a locked pointer. Currently there is
+# no way of escaping them.
+ht_text = st.text().filter(lambda s: '[' not in s and '$' not
+                                                   in s)
+
+# st.builds
+# st.recursive
+# st.composite
+
+# Can I pass an argument to a strategy? Yes, if I use draw(), but that way I
+# can't use @example. And the output is different, if I understand it right.
+# After each act(), I can put the available pointers (locked and unlocked) in
+# a bundle. Nah, but I can't delete anything from a bundle.
+
+# So I have to use a GenericStateMachine? But for that I can't use @example
+# etc. either.
+
+# So how do we make hypertext?
+# We generate a list with elements randomly chosen between text and expanded
+# pointers and unexpanded pointers.
+# Unexpanded pointers are chose from a parameter.
+# Expanded pointers consist of an opening bracket, hypertext and a closing
+# bracket.
+
+st.lists(st.one_of(ht_text, st.integers()))
+
+
+def ht_base(pointers):
+    return st.lists(ht_text | st.sampled_from(pointers)) \
+                .map(lambda l: " ".join(l))
+
+
+def expanded_pointer(ht):
+    return ht.map(lambda ht_: "{}".format(ht_))
+
+
+def hypertext(pointers):
+    return st.recursive(ht_base(pointers),
+                        lambda base: st.lists(base | expanded_pointer(
+                                base)).map(lambda l: " ".join(l)))
+
+
 
 class RandomExercise(RuleBasedStateMachine):
     def __init__(self):
