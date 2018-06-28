d24215923ecf0ad2563849f7c521d2cf32f6c8eb
Richard Möhn <richard.moehn@posteo.de>
2018-06-28 14:31:23 +0900

    test_hypothesis: Add unlock rule and pointers in hypertext

--- tests/test_hypothesis.py
+++ tests/test_hypothesis.py
@@ -1,14 +1,19 @@
+import copy
+from typing import Any, Optional
+
 import hypothesis.strategies as st
+from hypothesis.searchstrategy import SearchStrategy
 from hypothesis.stateful import precondition, rule, RuleBasedStateMachine
 
-from patchwork.actions import Reply
+from patchwork.actions import Reply, Unlock
+from patchwork.context import Context
 from patchwork.datastore import Datastore
 from patchwork.scheduling import RootQuestionSession, Scheduler
 
 
 # [ starts an expanded pointer and $ starts a locked pointer. Currently there is
 # no way of escaping them.
-ht_text = st.text().filter(lambda s: '[' not in s and '$' not
+ht_text = st.text(min_size=0).filter(lambda s: '[' not in s and '$' not
                                                    in s)
 
 # st.builds
@@ -48,28 +53,50 @@ def hypertext(pointers):
                                 base)).map(lambda l: " ".join(l)))
 
 
+def locked_unlocked_pointers(c: Context):
+    ul = copy.copy(c.unlocked_locations)
+    ul.discard(c.workspace_link)
+    up = [c.pointer_names[a] for a in ul]
+    lp = [p for p, a in c.name_pointers.items() if a not in ul]
+    return lp, up
+
 
 class RandomExercise(RuleBasedStateMachine):
     def __init__(self):
         super(RandomExercise, self).__init__()
-        self.db: Datastore              = None
-        self.sess: RootQuestionSession  = None
+        self.db: Optional[Datastore]                = None
+        self.sess: Optional[RootQuestionSession]    = None
 
     @precondition(lambda self: not self.sess)
-    @rule(root_question=hypertext)
-    def start_session(self, root_question: str):
+    @rule(data=st.data())
+    def start_session(self, data: SearchStrategy[Any]):
         self.db     = Datastore()
-        self.sess   = RootQuestionSession(Scheduler(self.db), root_question)
+        self.sess   = RootQuestionSession(Scheduler(self.db),
+                                          data.draw(hypertext([])))
 
     @precondition(lambda self: self.sess)
-    @rule(answer=hypertext,
+    @rule(data=st.data(),
           is_reset_db=st.booleans())
-    def reply(self, answer: str, is_reset_db: bool):
-        self.sess.act(Reply(answer))
+    def reply(self, data: SearchStrategy[Any], is_reset_db: bool):
+        context: Context = self.sess.current_context
+        pointers = context.name_pointers_for_workspace(context.workspace_link,
+                                                       self.db)
+        self.sess.act(Reply(data.draw(hypertext(list(pointers.keys())))))
         if self.sess.final_answer_promise:
             self.sess = None
             if is_reset_db:
                 self.db = None
+
+
+    # MAYBE TODO: Also try unlocking already unlocked pointers and assert
+    # that it causes an exception.
+    @precondition(lambda self: self.sess)
+    @rule(data=st.data())
+    def unlock(self, data: SearchStrategy[Any]):
+        lp, __ = locked_unlocked_pointers(self.sess.current_context)
+        sample_pointer = data.draw(st.sampled_from(lp))
+        self.sess.act(Unlock(sample_pointer))
+
 
 
 TestHypothesis = RandomExercise.TestCase
