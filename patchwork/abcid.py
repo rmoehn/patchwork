42c19eabc2fdb373f80a0117065a91b50e523c80
Richard Möhn <richard.moehn@posteo.de>
2018-07-05 08:39:42 +0900

    DEBUG Add Abcid

---
+++ patchwork/abcid.py
@@ -0,0 +1,30 @@
+import random
+
+
+class Abcid(object):
+    """
+    Readable alternative to UUID for debugging
+
+    Prep
+    ----
+    Download the word list::
+    $ cd <directory from which you run patchwork>
+    $ wget https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt
+    """
+
+    def __init__(self, seed=None):
+        self.path   = "words_alpha.txt"
+        self.words  = []
+        self.random = random.Random(seed)
+
+
+    def load_words(self):
+        with open(self.path, 'r') as f:
+            self.words = [s.strip() for s in f.readlines()]
+
+
+    def abcid(self):
+        chosen = []
+        while sum(len(w) for w in chosen) < 15:
+            chosen += self.random.choices(self.words, k=2)
+        return "-".join(chosen)
